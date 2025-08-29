67d76e2b43c5251c1c170fdfe4a02b51f4ee1ad8
Oxy24 <100385914+Oxy24@users.noreply.github.com>
2025-08-29 23:46:59 +0000

    Aggiornato index.html

--- api/login.js
+++ api/login.js
@@ -2,9 +2,12 @@ export default function handler(req, res) {
   const client_id = "87ae440dcbbb40699c7b76dbda41a9da";
   const redirect_uri = "https://audiolog-one.vercel.app/api/callback";
   const scope = "user-read-recently-played";
-  const state = Math.random().toString(36).substring(2,15);
+  const state = Math.random().toString(36).substring(2, 15);
 
-  const auth_url = `https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scope=${encodeURIComponent(scope)}&redirect_uri=${encodeURIComponent(redirect_uri)}&state=${state}`;
+  const auth_url = `https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scope=${encodeURIComponent(
+    scope
+  )}&redirect_uri=${encodeURIComponent(redirect_uri)}&state=${state}`;
 
-  res.redirect(auth_url);
+  res.writeHead(302, { Location: auth_url });
+  res.end();
 }
